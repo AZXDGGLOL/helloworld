07de867d03f0f59ab405850c9b4e5324bbbe612e
Anuntasit Singcha <archizombie12345@gmail.com>
2025-11-19 11:46:15 +0700

    peak

--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -9,27 +9,37 @@ namespace helloworld
             string name = "";
             int height = 0;
             int weight = 1;
-            String gender = ""; 
+            String gender = "";
             Console.WriteLine("Hello, World!");
             Console.WriteLine();
+
+            while (true) { 
             Console.WriteLine("Please, input you full name (Eng) :");
             name = Console.ReadLine();
+                if (name == "") ;
+                else break;
+            };
 
-            Console.WriteLine("Pleese tell us your gender f/m");
-            gender = Console.ReadLine();
 
+            while (true) {
+                Console.WriteLine("Plaese fill in your height(cm) : ");
+                height = Convert.ToInt16(Console.ReadLine());
+                if (height > 100); break; };
+            
 
-            Console.WriteLine("Plaese fill in your height(cm) : ");
-            height = Convert.ToInt16(Console.ReadLine());
-            if (gender == "f")
+            while (true)
             {
-                 weight = height -= 110;
-            }
-            else if (gender == "m")
-            {
-                 weight = height -= 100;
-            }
-
+                if (gender == "f")
+                {
+                    weight = height -= 110; break;
+                }
+                else if (gender == "m")
+                {
+                    weight = height -= 100; break;
+                }
+                else Console.WriteLine("Pleese tell us your gender f/m");
+                gender = Console.ReadLine();
+            };
 
             Console.WriteLine("Hello, Welcome " + name);
             Console.WriteLine("Your ideal weight is : " + weight.ToString());
