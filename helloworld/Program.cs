40b10f9a57958edf83b87e0f7503a3fe3649cdba
Anuntasit Singcha <archizombie12345@gmail.com>
2025-11-19 09:28:14 +0700

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Anuntasit Singcha");
+            Console.WriteLine("Computer science program");
+            Console.WriteLine("---------------++++--------------");
+            Console.WriteLine("My Home town is");
+            Console.WriteLine("Bueng Kan");
+            Console.WriteLine("Thailand");
+        }
+    }
+}
